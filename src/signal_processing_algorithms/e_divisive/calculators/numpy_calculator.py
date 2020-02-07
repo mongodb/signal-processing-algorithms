a19e411ac80bb11f9025a5fc57e9ef086ab02fc0
Alexander Costas <alexander.costas@mongodb.com>
2020-02-07 12:55:08 -0500

    intermediate

--- src/signal_processing_algorithms/e_divisive/calculators/numpy_calculator.py
+++ src/signal_processing_algorithms/e_divisive/calculators/numpy_calculator.py
@@ -3,29 +3,23 @@ import numpy as np
 
 
 def _calculate_q(
-    term1: np.ndarray, term2: np.ndarray, term3: np.ndarray, m: int, n: int
+        cross_term: np.ndarray, x_term: np.ndarray, y_term: np.ndarray, x_len: int, y_len: int
 ) -> np.ndarray:
     """
     Calculate the q value from the terms and coefficients.
 
-    :param term1: The current cumulative value for the first
-    term in the E-Divisive algorithm. This is the sum of the differences to
-    the right of the current location.
-    :param term2: The current cumulative value for the second
-    term in the E-Divisive algorithm. This is the sum of the differences to
-    the at the current location.
-    :param term3: The current cumulative value for the third
-    term in the E-Divisive algorithm. This is the sum of the differences to
-    the left of the current location.
-    :param m: The current row location.
-    :param n: The current column location.
+    :param cross_term: The sum of differences between sample distributions X, Y.
+    :param x_term: The sum of differences within sample distribution X.
+    :param y_term: The sum of differences within sample distribution Y.
+    :param x_len: The length of sample distribution X.
+    :param y_len: The length of sample distribution Y.
 
     :return: The q value generated from the terms.
     """
-    term1_reg = term1 * (2.0 / (m * n))
-    term2_reg = term2 * (2.0 / (n * (n - 1)))
-    term3_reg = term3 * (2.0 / (m * (m - 1)))
-    newq = (m * n // (m + n)) * (term1_reg - term2_reg - term3_reg)
+    term1_reg = cross_term * (2.0 / (x_len * y_len))
+    term2_reg = x_term * (2.0 / (y_len * (y_len - 1)))
+    term3_reg = y_term * (2.0 / (x_len * (x_len - 1)))
+    newq = (x_len * y_len // (x_len + y_len)) * (term1_reg - term2_reg - term3_reg)
     return newq
 
 
@@ -48,32 +42,25 @@ def calculate_qhat_values(diffs: np.ndarray) -> np.ndarray:
     :param diffs: The difference matrix.
     :return: The qhat values.
     """
-    length = len(diffs)
-    qhat_values = np.zeros(length, dtype=np.float)
+    qhat_values = np.zeros(len(diffs), dtype=np.float)
 
-    if length < 5:
-        return qhat_values
+    # We will partition our signal into:
+    # X = {Xi; 0 <= i < tau}
+    # Y = {Yj; tau <= j < len(signal) }
+    # and look for argmax(tau)Q(tau)
+    tau = 0
 
-    n = 2
-    m = length - n
+    # sum |Xi - Yj| for i < tau <= j
+    cross_term = np.sum(diffs[:tau, tau:])
+    # sum |Xi - Xj| for i < j < tau
+    x_term = np.sum(np.triu(diffs[:tau, :tau]))
+    # sum |Yi - Yj| for tau <= i < j
+    y_term = np.sum(np.triu[tau:, tau:])
 
-    # Each line is preceded by the equivalent list comprehension.
+    qhat_values[tau] = _calculate_q(cross_term, x_term, y_term, tau, len(diffs) - tau)
 
-    # term1 = sum(diffs[i][j] for i in range(n) for j in range(n, self.window))
-    term1 = np.sum(diffs[:n, n:])
-
-    # term2 = sum(diffs[i][k] for i in range(n) for k in range(i + 1, n))
-    term2 = np.sum(np.triu(diffs[:n, :n], 0))
-
-    # term3 = sum(diffs[j][k] for j in range(n, self.window)
-    #                         for k in range(j + 1, self.window))
-    term3 = np.sum(np.triu(diffs[n:, n + 1 :], 0))
-
-    qhat_values[n] = _calculate_q(term1, term2, term3, m, n)
-
-    for n in range(3, (length - 2)):
-        m = length - n
-        column_delta = np.sum(diffs[n - 1, : n - 1])
+    for tau in range(1, len(diffs)):
+        column_delta = np.sum(diffs[tau - 1, : tau - 1])
         row_delta = np.sum(diffs[n:, n - 1])
 
         term1 = term1 - column_delta + row_delta
