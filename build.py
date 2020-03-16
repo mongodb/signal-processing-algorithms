9955a3642ad1b363e7a8845b05218f40b5c1ee90
Alexander Costas <alexander.costas@mongodb.com>
2020-03-16 14:07:57 -0400

    intermediate

--- build.py
+++ build.py
@@ -1,8 +1,6 @@
-from distutils import build_ext
 from sys import platform
 
 from setuptools import Extension
-from setuptools.errors import DistutilsPlatformError
 
 ext_modules=[
     Extension(
@@ -14,29 +12,10 @@ ext_modules=[
     )
 ]
 
-class BuildFailed(Exception):
-    pass
-
-
-class ExtBuilder(build_ext):
-
-    def run(self):
-        try:
-            build_ext.run(self)
-        except (DistutilsPlatformError, FileNotFoundError):
-            raise BuildFailed('File not found. Could not compile C extension.')
-
-    def build_extension(self, ext):
-        try:
-            build_ext.build_extension(self, ext)
-        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
-            raise BuildFailed('Could not compile C extension.')
-
-
 def build(setup_kwargs):
     """
     This function is mandatory in order to build the extensions.
     """
     setup_kwargs.update(
-        {"ext_modules": ext_modules, "cmdclass": {"build_ext": ExtBuilder}}
+        {"ext_modules": ext_modules}
     )
