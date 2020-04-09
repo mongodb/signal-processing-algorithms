69beacaeee8ed3c47e1497f63c287f23dabf0453
Alexander Costas <alexander.costas@mongodb.com>
2020-04-09 19:01:43 -0400

    intermediate

---
+++ docs/profiling/numpy_calculator.py
@@ -0,0 +1,30 @@
+import numpy as np
+from signal_processing_algorithms.e_divisive.base import EDivisiveCalculator
+from signal_processing_algorithms.e_divisive.calculators import numpy_calculator as EDivisive
+
+
+class NumpyCalculator(EDivisiveCalculator):
+    """
+    Numpy O(n^2) implementation.
+    """
+
+    @staticmethod
+    def calculate_diffs(series: np.ndarray) -> np.ndarray:
+        return EDivisive.calculate_diffs(series)
+
+    @staticmethod
+    def calculate_qhat_values(diffs: np.ndarray) -> np.ndarray:
+        length = len(diffs)
+        qhat_values = np.zeros(len(diffs), dtype=np.float)
+        if length < 5:
+            return qhat_values
+
+        for n in range(2, length - 2):
+            m = length - n
+
+            term1 = np.sum(diffs[:n, n:])
+            term2 = np.sum(np.triu(diffs[:n, :n], 0))
+            term3 = np.sum(np.triu(diffs[n:, n + 1:], 0))
+
+            qhat_values[n] = EDivisive._calculate_q(term1, term2, term3, m, n)
+        return qhat_values
---
+++ docs/profiling/windowed_calculator.py
@@ -0,0 +1,38 @@
+import numpy as np
+from signal_processing_algorithms.e_divisive.base import EDivisiveCalculator
+from signal_processing_algorithms.e_divisive.calculators import numpy_calculator as EDivisive
+
+
+class WindowedCalculator(EDivisiveCalculator):
+    @staticmethod
+    def calculate_diffs(series: np.ndarray) -> np.ndarray:
+        return EDivisive.calculate_diffs(series)
+
+    @staticmethod
+    def calculate_qhat_values(diffs: np.ndarray) -> np.ndarray:
+        length = len(diffs)
+        qhat_values = np.zeros(len(diffs), dtype=np.float)
+        if length < 5:
+            return qhat_values
+
+        window = int(round(length / 2))
+
+        for n in range(2, length - 2):
+            m = length - n
+
+            term1 = sum(
+                diffs[i][j]
+                for i in range(max(0, n - window + 1), n)
+                for j in range(n, min(length, n + window))
+            )
+            term2 = sum(
+                diffs[i][k] for i in range(max(0, n - window + 1), n) for k in range((i + 1), n)
+            )
+            term3 = sum(
+                diffs[j][k]
+                for j in range(n, min(length, n + window + 1))
+                for k in range((j + 1), min(length, n + window + 1))
+            )
+
+            qhat_values[n] = EDivisive._calculate_q(term1, term2, term3, m, n)
+        return qhat_values
