c8ed1b8448b5f6703a6883a3e46ab5bf232cc52d
David Bradford <david.bradford@mongodb.com>
2020-05-14 11:04:08 -0400

    MAKE-1329: Create deployable C extension (#4)

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+# This uses 'manylinux' docker image and 'auditwheel' to ensure our c extension
+# works on lots of linux platforms without needing to be recompiled. The image
+# provides toolchain for various version. We will be using the 3.7 version which
+# is at the path below.
+#
+# For more information see: https://github.com/pypa/auditwheel
+export PATH="$PATH:/opt/python/cp37-cp37m/bin"
+
+poetry build
+find ./dist -name "*.whl" | xargs auditwheel repair
+rm dist/*.whl
+mv wheelhouse/* dist
+poetry publish --username $PYPI_USERNAME --password $PYPI_PASSWORD
